45631dea90e8a4313b0205962e1e281ec627cfe9
Marcondes Amorim <marcondesjra@gmail.com>
2019-07-23 11:36:26 -0300

    added slide select

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,8 +17,17 @@ function passaSlide() {
     slideItem++;
   }
 
+  
   document.getElementsByClassName("slideshow_area")[0].style.marginLeft =
     "-" + slidewidth * slideItem + "px";
+  var objs = document.getElementsByClassName("button");
+  for (var i=0;i < objs.length; i++){
+    if (i == slideItem){
+      objs[i].style.backgroundColor = "black";
+    } else {
+      objs[i].style.backgroundColor = "gray";
+    };
+  }
 }
 function changeSlide(pos) {
   slideItem = pos;
@@ -26,4 +35,13 @@ function changeSlide(pos) {
   var slidewidth = document.getElementById("slideshow").offsetWidth;
   document.getElementsByClassName("slideshow_area")[0].style.marginLeft =
     "-" + slidewidth * slideItem + "px";
+    var objs = document.getElementsByClassName("button");
+    for (var i=0;i < objs.length; i++){
+      if (i == pos){
+        objs[i].style.backgroundColor = "black";
+      } else {
+        objs[i].style.backgroundColor = "gray";
+      };
+    }
+  
 }
